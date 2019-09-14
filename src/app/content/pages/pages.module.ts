e4a1d17f4ebc8abc4ae9b46d2f2bc23405e494f0
Lucas Dias <lucas-dias@hotmail.com>
2019-09-14 10:46:54 -0300

    first commit

---
+++ src/app/content/pages/pages.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PagesPage } from './pages.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesPage
+  },
+   { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)},
+
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [PagesPage]
+})
+export class PagesPageModule {}
